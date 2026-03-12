e3ba1970f32df25cb9ffe1b4a26bbad4cda44ff3
Maximo Ariel Aranibe <darknesswong@gmail.com>
2026-03-11 22:29:41 -0300

    fix import case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import React from 'react';
-import WhatsAppButton from './components/WhatsappButton';
+import WhatsAppButton from './components/WhatsAppButton';
 import ShowVideos from './components/ShowVideos';
 import ScrollToTop from './utils/ScrollToTop';
 
